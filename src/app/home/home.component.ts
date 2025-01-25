382072119dbd7c5dbd0652d9ecab655ac6b920e1
Nina <vrankovic.nina00@gmail.com>
2025-01-26 00:56:11 +0100

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+    <h1>
+      Welcome to my app!
+    </h1>
+
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
